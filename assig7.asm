3fb9a1b3e259507a9f30374a2466822453824024
kruthii01 <kruthi.23cs059@sode-edu.in>
2025-04-08 19:06:44 +0530

    assignment question 7

---
+++ assig7.asm
@@ -0,0 +1,10 @@
+;Write an ARM assembly program to demonstrate the usage of the MRS and  MSR instruction.
+
+  AREA Demo, CODE, READONLY  ; Define a code section named "Demo" as read-only  
+START
+    LDR R0, =0x10          ; Load the immediate value 0x10 into register R0  
+    MSR CPSR_c, R0         ; Move the value in R0 to the CPSR (changing processor mode)  
+    MRS R1, CPSR           ; Move the current CPSR value into R1 (read status register)  
+    MOV R2, R1             ; Copy the CPSR value from R1 to R2  
+HERE B HERE                ; Infinite loop to halt execution  
+    END                    ; End of the program
